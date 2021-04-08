908e2cd4076def5a5e640e75a02d567f473418e7
Farida <fshka@mail.ru>
2021-04-08 09:33:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let title=document.getElementsByClassName('prod_item_title');
+console.log(title[0].innerHTML);
+
+
+
+let price=document.getElementsByClassName('prod_item_price');
+console.log(price[0].innerText);
+
+
+
+let image=document.getElementsByClassName('prod_item_img');
+console.log(image[0].getElementsByTagName('a'));
+
+
+
+
+
+
